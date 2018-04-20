ea4d13dc804165e745625498c999b283fa908c64
Muhammad Usman <muhammad@usmanity.com>
2018-04-20 14:16:51 -0700

    adding a few more things

---
+++ src/components/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>
+      <img src="../assets/bee.png" class="bee">
+      {{ msg }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+.bee {
+  width: 30px;
+  height: 30px;
+}
+</style>
